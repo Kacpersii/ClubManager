bae69f5cf6c96dcb1bc5cea89986fcd8d25d41d5
kacper <kacper261198@gmail.com>
2021-01-18 16:30:51 +0100

    Update models, add ClubController

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClubManager.DAL;
+using ClubManager.Models;
+
+namespace ClubManager.Controllers
+{
+    [Authorize]
+    public class ClubController : Controller
+    {
+        private ClubManagerContext db = new ClubManagerContext();
+
+        // GET: Club
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View(db.Clubs.ToList());
+        }
+
+        // GET: Club/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // GET: Club/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Club/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Stadium,Logo")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clubs.Add(club);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(club);
+        }
+
+        // GET: Club/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: Club/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Stadium,Logo")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(club).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(club);
+        }
+
+        // GET: Club/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: Club/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Club club = db.Clubs.Find(id);
+            db.Clubs.Remove(club);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
