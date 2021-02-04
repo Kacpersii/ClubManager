5dc56f6887d07993690088d75b0daa2136950fc5
kacper <kacper261198@gmail.com>
2021-02-04 18:54:16 +0100

    PlayerController - index and details

--- Controllers/CoachController.cs
+++ Controllers/CoachController.cs
@@ -11,133 +11,116 @@ using ClubManager.Models;
 
 namespace ClubManager.Controllers
 {
-
-    [Authorize]
-    public class PlayerController : Controller
+    public class CoachController : Controller
     {
         private ClubManagerContext db = new ClubManagerContext();
 
-        // GET: Player
-        [Authorize(Roles = "Admin,Manager")]
+        // GET: Coach
         public ActionResult Index()
         {
-            var players = db.Players.Include(p => p.Club).Include(p => p.Team).Include(p => p.User);
-            return View(players.ToList());
+            var coaches = db.Coaches.Include(c => c.Club).Include(c => c.User);
+            return View(coaches.ToList());
         }
 
-        // GET: Player/Details/5
-        [Authorize(Roles = "Admin,Manager")]
+        // GET: Coach/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Player player = db.Players.Find(id);
-            if (player == null)
+            Coach coach = db.Coaches.Find(id);
+            if (coach == null)
             {
                 return HttpNotFound();
             }
-            return View(player);
+            return View(coach);
         }
 
-        [Authorize(Roles = "Player")]
-        public ActionResult MyProfile()
-        {
-            User user = db.Users.Single(u => u.UserName == User.Identity.Name);
-            Player player = db.Players.Single(p => p.UserID == user.ID);
-            
-            return View("Details", player);
-        }
-
-        // GET: Player/Create
+        // GET: Coach/Create
         public ActionResult Create()
         {
             ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name");
-            ViewBag.TeamID = new SelectList(db.Teams, "ID", "Name");
             ViewBag.UserID = new SelectList(db.Users, "ID", "UserName");
             return View();
         }
 
-        // POST: Player/Create
+        // POST: Coach/Create
         // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
         // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,UserID,ClubID,TeamID,LeadingLeg,Height,Weight,ShirtsNumber,MainPosition,SecondPosition")] Player player)
+        public ActionResult Create([Bind(Include = "ID,UserID,ClubID")] Coach coach)
         {
             if (ModelState.IsValid)
             {
-                db.Players.Add(player);
+                db.Coaches.Add(coach);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", player.ClubID);
-            ViewBag.TeamID = new SelectList(db.Teams, "ID", "Name", player.TeamID);
-            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", player.UserID);
-            return View(player);
+            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", coach.ClubID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", coach.UserID);
+            return View(coach);
         }
 
-        // GET: Player/Edit/5
+        // GET: Coach/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Player player = db.Players.Find(id);
-            if (player == null)
+            Coach coach = db.Coaches.Find(id);
+            if (coach == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", player.ClubID);
-            ViewBag.TeamID = new SelectList(db.Teams, "ID", "Name", player.TeamID);
-            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", player.UserID);
-            return View(player);
+            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", coach.ClubID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", coach.UserID);
+            return View(coach);
         }
 
-        // POST: Player/Edit/5
+        // POST: Coach/Edit/5
         // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
         // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,UserID,ClubID,TeamID,LeadingLeg,Height,Weight,ShirtsNumber,MainPosition,SecondPosition")] Player player)
+        public ActionResult Edit([Bind(Include = "ID,UserID,ClubID")] Coach coach)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(player).State = EntityState.Modified;
+                db.Entry(coach).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", player.ClubID);
-            ViewBag.TeamID = new SelectList(db.Teams, "ID", "Name", player.TeamID);
-            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", player.UserID);
-            return View(player);
+            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", coach.ClubID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", coach.UserID);
+            return View(coach);
         }
 
-        // GET: Player/Delete/5
+        // GET: Coach/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Player player = db.Players.Find(id);
-            if (player == null)
+            Coach coach = db.Coaches.Find(id);
+            if (coach == null)
             {
                 return HttpNotFound();
             }
-            return View(player);
+            return View(coach);
         }
 
-        // POST: Player/Delete/5
+        // POST: Coach/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Player player = db.Players.Find(id);
-            db.Players.Remove(player);
+            Coach coach = db.Coaches.Find(id);
+            db.Coaches.Remove(coach);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
