269827edc5e9ff2010e2a82e12c6ae5af8bfdd04
kacper <kacper261198@gmail.com>
2021-02-17 17:49:32 +0100

    Coach - create

--- Controllers/CoachController.cs
+++ Controllers/CoachController.cs
@@ -8,6 +8,9 @@ using System.Web;
 using System.Web.Mvc;
 using ClubManager.DAL;
 using ClubManager.Models;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace ClubManager.Controllers
 {
@@ -15,6 +18,19 @@ namespace ClubManager.Controllers
     public class CoachController : Controller
     {
         private ClubManagerContext db = new ClubManagerContext();
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
 
         // GET: Coach
         [Authorize(Roles = "Admin")]
@@ -61,10 +77,23 @@ namespace ClubManager.Controllers
         }
 
         // GET: Coach/Create
+        [Authorize(Roles = "Admin,Manager")]
         public ActionResult Create()
         {
-            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name");
-            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName");
+            if (User.IsInRole("Admin"))
+            {
+                ViewBag.UserID = new SelectList(db.Users, "ID", "UserName");
+                ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name");
+            }
+            else
+            {
+                User user = db.Users.Single(u => u.UserName == User.Identity.Name);
+                Manager manager = db.Managers.Single(m => m.UserID == user.ID);
+
+                var coachesInOtherClubs = db.Coaches.Where(c => c.ClubID != manager.ClubID);
+                var users = db.Users.Where(u => !coachesInOtherClubs.Any(c => c.UserID == u.ID));
+                ViewBag.UserID = new SelectList(users, "ID", "UserName");
+            }
             return View();
         }
 
@@ -73,21 +102,56 @@ namespace ClubManager.Controllers
         // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,UserID,ClubID")] Coach coach)
+        [Authorize(Roles = "Admin,Manager")]
+        public ActionResult Create(Coach coach)
         {
-            if (ModelState.IsValid)
+            if(User.IsInRole("Admin"))
             {
-                db.Coaches.Add(coach);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                if (ModelState.IsValid)
+                {
+                    db.Coaches.Add(coach);
+                    db.SaveChanges();
 
-            ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", coach.ClubID);
-            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", coach.UserID);
+                    var userModel = db.Users.Find(coach.UserID);
+                    var user1 = UserManager.FindByName(userModel.UserName);
+                    UserManager.AddToRole(user1.Id, "Coach");
+                    db.SaveChanges();
+
+                    return RedirectToAction("Details", new { id = coach.ID });
+                }
+
+                ViewBag.ClubID = new SelectList(db.Clubs, "ID", "Name", coach.ClubID);
+                ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", coach.UserID);
+            }
+            else
+            {
+                User user = db.Users.Single(u => u.UserName == User.Identity.Name);
+                Manager manager = db.Managers.Single(m => m.UserID == user.ID);
+
+                if (ModelState.IsValid)
+                {
+                    coach.ClubID = manager.ClubID;
+                    db.Coaches.Add(coach);
+                    db.SaveChanges();
+
+                    var userModel = db.Users.Find(coach.UserID);
+                    var user1 = UserManager.FindByName(userModel.UserName);
+                    UserManager.AddToRole(user1.Id, "Coach");
+                    db.SaveChanges();
+
+                    return RedirectToAction("Details", new { id = coach.ID });
+                }
+
+                var coachesInOtherClubs = db.Coaches.Where(c => c.ClubID != manager.ClubID);
+                var users = db.Users.Where(u => !coachesInOtherClubs.Any(c => c.UserID == u.ID));
+                ViewBag.UserID = new SelectList(users, "ID", "UserName");
+            }
+            
             return View(coach);
         }
 
         // GET: Coach/Edit/5
+        [Authorize(Roles = "Admin,Manager")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -109,6 +173,7 @@ namespace ClubManager.Controllers
         // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Manager")]
         public ActionResult Edit([Bind(Include = "ID,UserID,ClubID")] Coach coach)
         {
             if (ModelState.IsValid)
@@ -123,6 +188,7 @@ namespace ClubManager.Controllers
         }
 
         // GET: Coach/Delete/5
+        [Authorize(Roles = "Admin,Manager")]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -140,6 +206,7 @@ namespace ClubManager.Controllers
         // POST: Coach/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Manager")]
         public ActionResult DeleteConfirmed(int id)
         {
             Coach coach = db.Coaches.Find(id);
